bb228eb44c4456062c6539425cda583b7503f2dd
lobokryt77777@gmail.com <lobokryt77777@gmail.com>
2020-03-13 19:40:20 +0300

    h

--- SolovyovBluetooth/Form1.cs
+++ SolovyovBluetooth/Form1.cs
@@ -6,9 +6,9 @@ using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;\
+using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Text
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
